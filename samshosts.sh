8f4de8e519df3423db96cebed2c8ff0857ed1e01
Sammy Lord (Holder Account) <163201376+sammylord-holder@users.noreply.github.com>
2025-06-10 20:16:49 -0500

    Update samshosts.sh

--- samshosts.sh
+++ samshosts.sh
@@ -5,8 +5,6 @@ curl https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/hosts/pro.txt >> h
 printf "\n" >> hosts
 curl https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews/hosts >> hosts
 printf "\n" >> hosts
-curl https://someonewhocares.org/hosts/hosts >> hosts
-printf "\n" >> hosts
 crul https://raw.githubusercontent.com/jerryn70/GoodbyeAds/master/Extension/GoodbyeAds-Spotify-AdBlock.txt >> hosts
 printf "\n" >> hosts
 curl https://raw.githubusercontent.com/jerryn70/GoodbyeAds/master/Extension/GoodbyeAds-YouTube-AdBlock.txt >> hosts
